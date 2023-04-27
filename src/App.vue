52ba396e1a96ee517f4b679e654376280924a370
Rick <ri.plazar@duocuc.cl>
2023-04-27 01:19:28 -0400

    se agrega el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/allo.png">
+  <HelloWorld/>
+  <MiComponente></MiComponente>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import MiComponente from './components/MiComponente.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
 }
 </script>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <div id="mi-componente">
+        <h1></h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+
+        
+    
+}
+</script>
